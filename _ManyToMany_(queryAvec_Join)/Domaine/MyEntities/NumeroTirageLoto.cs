3cb2ffa294607d52b59b87f8ed2a74987c01537e
Richard PAYET <rick.info.rsoc@gmail.com>
2022-10-07 20:21:33 +0200

    _ManyToMany_(queryAvec_Join)

---
+++ _ManyToMany_(queryAvec_Join)/Domaine/MyEntities/NumeroTirageLoto.cs
@@ -0,0 +1,13 @@
+﻿namespace Domaine.MyEntities
+{
+    public class NumeroTirageLoto 
+    {
+        public int Id { get; set; }
+
+        public int NumeroId { get; set; }
+        //public Numero Numero { get; set; } //Non, pose problème de réf. circulaire lorsque je fais un Include() dessus, ET que je veux jsonify ce résultat tel quel.
+
+        public int TirageLotoId { get; set; }
+        //public TirageLoto TirageLoto { get; set; } //Non, pose problème de réf. circulaire lorsque je fais un Include() dessus, ET que veux jsonify ce résultat tel quel.
+    }
+}
